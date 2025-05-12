7a507ad6ee1ab85439ceaac5fcd354e0938b6ee2
wojteksuj <s31087@pjwstk.edu.pl>
2025-05-12 19:59:55 +0200

    Added rowversion field to all models

--- src/Device.Models/Embedded.cs
+++ src/Device.Models/Embedded.cs
@@ -8,6 +8,7 @@ public class Embedded : Device
     public string NetworkName { get; set; }
     private string _ipAddress;
     public bool IsConnected { get; set; }
+    private int rowVersion {get;set;}
 
     public string IpAddress
     {
